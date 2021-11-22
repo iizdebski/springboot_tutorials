095223347c0a2364cc2fcd92db8b8161597393bf
SwAppCoder <iizdepskyy112@gmail.com>
2021-11-22 17:33:34 +0200

    change_context_path_of_web_app

---
+++ 16.12.2018_change_context_path_of_web_app/src/main/java/com/izdebski/SpringBootChangeServerPortWebApp/EmbeddedServletContainerCustomizerBean.java
@@ -0,0 +1,15 @@
+package com.izdebski.SpringBootChangeServerPortWebApp;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmbeddedServletContainerCustomizerBean implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(7171);
+        container.setContextPath("/mywebapp");
+    }
+}
